da6ffdbfa86c9482983869970671a3495edb8f98
Henrique Lopes Dionizio Muniz <henrique.muniz@iesb.edu.br>
2026-04-20 13:39:44 -0300

    Gerar resultados dos experimentos

---
+++ src/include/relatorio.h
@@ -0,0 +1,7 @@
+#ifndef relatorio_h
+#define relatorio_h
+
+void salvar_relatorio(int quantidade, double* tempos, int num_ciclos, int buscas_por_ciclo,
+                      int ids_inicio, int ids_meio, int ids_fim, int ids_inexistentes);
+
+#endif
